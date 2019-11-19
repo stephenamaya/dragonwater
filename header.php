665c00f8d34721c361141678b8ab3ecdd00bb842
stephenamaya <57957346+stephenamaya@users.noreply.github.com>
2019-11-19 11:35:52 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dragon Water</title>
+<link rel="stylesheet" type="text/css" href="main.css" />
+<link href="https://fonts.googleapis.com/css?family=Capriola&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Heebo:100,300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="login.php">Login</a></li>
+    <li style="float:right"><a href="signup.php">Sign up</a></li>
+  </ul>
