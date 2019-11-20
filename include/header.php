82d45346c9b6d8cf725720d66a3f9c649339b8cc
stephenamaya <57957346+stephenamaya@users.noreply.github.com>
2019-11-20 14:08:22 -0500

    bottle designs
    designs with css for the grid

--- include/header.php
+++ include/header.php
@@ -11,6 +11,7 @@
   <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About</a></li>
+    <li><a href="store.php">Store</a></li>
     <li><a href="login.php">Login</a></li>
     <li style="float:right"><a href="signup.php">Sign up</a></li>
   </ul>
---
+++ store.php
@@ -0,0 +1,26 @@
+<?php include "include/header.php";?>
+
+<h1 class ="welcome">
+  STORE
+</h1>
+<br><br><br><br><br>
+
+
+
+<div class="storegrid">
+	<div class = "column">
+   <img src="img/kiwi.png" class = "watercan">
+	<h3>Kiwi</h3>
+	</div>
+
+	<div class = "column">
+   <img src="img/lemon.png" class = "watercan">
+	 <h3>Lemon</h3>
+	</div>
+
+	<div class = "column">
+	 <img src="img/raspberry.png" class = "watercan">
+	 <h3>Raspberry</h3>
+	</div>
+
+</div>
