5fcb4fa5bda7720c26c647a9b13fb7fba8e9a76e
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2019-11-20 20:24:39 -0500

    About page
    The profile pics, they jiggle.

--- about.php
+++ about.php
@@ -1,5 +1,25 @@
 <?php include "include/header.php";?>
 
-<h1 class ="welcome">
-  ABOUT US
-</h1>
+<h1 class ="welcome">  ABOUT US  </h1>
+<p>We like our water in cans.</p>
+
+<div class="grid-container">
+          <div class="grid-item">
+            <img src="img/tina.jpg" alt="Tina" class="img-about">
+            <h3>Tina Fellsberry</h3>
+            <p>I once drank water in a bottle and threw up.</p>
+            <h3>CEO</h3>
+          </div>
+
+          <div class="grid-item">
+            <img src="img/bob.jpg" alt="bob" class="img-about">
+            <h3>Bob Traichovsoki</h3>
+            <p>I love water in cans because you can recylce them into guns.</p>
+            <h3>HR Manager</h3>
+          </div>
+
+</div>
+
+
+</body>
+</html>
