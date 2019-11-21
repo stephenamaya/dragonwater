f7c49315335c130e5b772658f7665cbd2ac42fef
stephenamaya <57957346+stephenamaya@users.noreply.github.com>
2019-11-21 12:01:48 -0500

    login form
    login form

--- login.php
+++ login.php
@@ -3,3 +3,13 @@
 <h1 class ="welcome">
   LOG IN
 </h1>
+
+
+<br><br><br>
+
+
+<form action="logincheck.php" method = "POST">
+<p><input type="text" placeholder="Your username" name = "username"></p>
+<p><input type="password" placeholder="Your password" name = "password"></p>
+<p><input type="submit" name = "submit" value="Login!" ></p>
+</form>
