0a628563031be474894ab965f2b6e6530a41f473
stephenamaya <57957346+stephenamaya@users.noreply.github.com>
2019-11-27 18:43:25 -0500

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -6,4 +6,3 @@
 	$database = "dragonwater";
 
 	$con = mysqli_connect("localhost" , "$username" , "$password", "$database") or die(mysql_error());  //(host,username,password,) Connects to mysql server. Throws error if it cannot connect.
-?>
