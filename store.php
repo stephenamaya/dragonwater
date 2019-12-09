88099c5548d4dc65962cafec61390da4a07204db
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2019-12-08 19:37:20 -0500

    Removed unused and added alcohol

--- store.php
+++ store.php
@@ -5,8 +5,21 @@
 </h1>
 <br><br><br><br>
 
+<?php
+if(isset($_SESSION['username'])){
+  $result = mysqli_query($con, 'SELECT age FROM users WHERE username="'.$_SESSION['username'].'"') or die ("could not connect to mysql");
+  if(mysqli_num_rows($result) > 0 ){
+     while ($row = mysqli_fetch_array ($result)) {
+       if($row["age"]>=21){
+         echo "<p>Since you are over 21, all our Dragon Water products now of the option to contain alcohol!</p>";
+       }
+     }
+  }
+}
 
-
+ ?>
+<br>
+<br>
 <div class="storegrid">
 	<div class = "column">
    <img src="img/kiwi.png" class = "watercan">
@@ -44,6 +57,8 @@ echo "<div class='storegrid'>
 	 <img src='img/darkRaspberry.png' class = 'watercan'>
 	 <h3>Dark Raspberry</h3>
 	</div>";
+
+
 }
 ?>
 <br>
